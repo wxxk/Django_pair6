f3c3dfbac5f8dc18a521cbad1b465c63e7973af6
wxxk <dwde22@gmail.com>
2022-11-01 10:37:47 +0900

    ADD Accounts signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 app_name = "accounts"
-urlpatterns = []
+urlpatterns = [
+    path("signup/", views.signup, name="signup"),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.isvalid():
+            form.save()
+            return redirect("articles:index")
+    else:
+        form = CustomUserCreationForm()
+    context = {"form": form}
+    return render(request, "accounts/signup.html", context)
