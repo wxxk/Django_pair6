4ce1fe00f4784d0fd335680f21574a9fcddb7068
wxxk <dwde22@gmail.com>
2022-11-03 17:58:49 +0900

    ADD card [ALL]

--- community/urls.py
+++ community/urls.py
@@ -5,12 +5,5 @@ app_name = "community"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("create/", views.create, name="create"),
-    path("<int:pk>/detail/", views.detail, name="detail"),
-    path("<int:pk>/update/", views.update, name="update"),
-    path("<int:pk>/delete/", views.delete, name="delete"),
-    path('<int:pk>/comment/create/', views.comment_create, name='comment_create'),
-    path('<int:community_pk>/<comment_pk>/delete/', views.comment_delete, name='comment_delete'),
-    path('<int:pk>/like/', views.like, name='like'),
-    path('rule/', views.rule, name='rule'),
+    path("rule/", views.rule, name="rule"),
 ]
