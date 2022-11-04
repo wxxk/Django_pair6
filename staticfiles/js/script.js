3068d2b0c99137fa7c33383c1a08e9ca4c271a28
wxxk <dwde22@gmail.com>
2022-11-04 15:44:49 +0900

    first heroku

---
+++ staticfiles/js/script.js
@@ -0,0 +1,35 @@
+// var swiper = new Swiper(".slide-container", {
+//   slidesPerView: 6,
+//   spaceBetween: 30,
+//   slidesPerGroup: 1,
+//   loop: true,
+//   centerSlide: "true",
+//   grabCursor: "true",
+//   fade: "true",
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//     dynamicBullets: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+var swiper = new Swiper(".slide-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
