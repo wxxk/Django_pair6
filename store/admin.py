4975db06c8d02953f4c4c90d991cf1098917b000
wxxk <dwde22@gmail.com>
2022-11-07 16:40:57 +0900

    admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from accounts.models import User
+from store.models import Store
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Store)
